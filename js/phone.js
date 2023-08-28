db7607db6a3c344c1c92ef98a5a6dc900373d2d4
jarif181 <islamzim73@gmail.com>
2023-08-28 19:34:42 +0600

    first commit

--- js/phone.js
+++ js/phone.js
@@ -32,9 +32,10 @@ const displayPhones = (phones,isshowAll) =>{
                 <figure><img src="${phone.image}" /></figure>
                 <div class="card-body">
                 <h2 class="card-title">${phone.phone_name}</h2>
-                <p>${phone.slug}</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis qui ipsa animi, aperiam vero quos vitae tempora, 
+                </p>
+                <div class="card-actions justify-center">
+                    <button onclick="showDetail('${phone.slug}') ;" class="btn btn-primary">Show Details</button>
                 </div>
                 </div>
          `
@@ -43,7 +44,34 @@ const displayPhones = (phones,isshowAll) =>{
     lodinngSpenner(false)
 
 }
+const showDetail = async (id) =>{
+   
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json();
+    const allda = data.data
+    console.log(allda)
+    dataAllPhone(allda)
+
+}
  
+const dataAllPhone = (phone) => {
+    modalClick.showModal()
+    const setName= document.getElementById('allDetailsPhone')
+                setName.innerHTML = `
+                <img class="mb-2 md/lg:mb-5" src="${phone.image}" alt="">
+                <p><span class="text-xl mb-1">Brand: </span>${phone.brand}</p>
+                <p><span class="text-xl mb-1">Storage: </span>${phone.mainFeatures.storage}</p>
+                <p><span class="text-xl mb-1">Display Size: </span>${phone.mainFeatures.displaySize}</p>
+                <p><span class="text-xl mb-1">Chipset : </span>${phone.mainFeatures.chipSet}</p>
+                <p><span class="text-xl mb-1">Memory: </span>${phone.mainFeatures.memory}</p>
+                <p><span class="text-xl mb-1">Slug: </span>${phone.slug}}</p>
+                <p><span class="text-xl mb-1">Release data: </span>${phone.releaseDate}</p>
+
+                `
+}
+
+
+
 const search = (isshowAll) => {
     lodinngSpenner(true)
     const searchFild = document.getElementById('Search');
